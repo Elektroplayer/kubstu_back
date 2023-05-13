84deab32f496e9ecd507c280839c3fd102d25208
Electroplayer <vitaly.hawanski@yandex.ru>
2023-05-13 10:54:08 +0300

    INITAL COMMIT

---
+++ src/app.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+interface ICheckedData {
+  ok: boolean;
+  message?: string;
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/check_data')
+  checkData(@Query() query): ICheckedData {
+    // query.group query.inst_id query.token?
+
+    if (!query.group)
+      return {
+        ok: false,
+        message: 'Не указана группа',
+      };
+
+    if (!query.inst_id)
+      return {
+        ok: false,
+        message: 'Не указан институт',
+      };
+
+    if (query.group == '22-КБ-ИВ1')
+      return {
+        ok: false,
+        message: 'Эта группа в бане',
+      };
+
+    return {
+      ok: true,
+    };
+  }
+}
